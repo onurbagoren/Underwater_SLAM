de40c7885eec6522851ac3b0677d622cb70ebad8
Onur Bagoren <onurbagoren3@gmail.com>
2022-03-20 18:27:34 -0400

    Several issues: - Need to figure out how to involve the integration of the IMU - The timestamps from InEKF are the same for some indexes, this is likely because the "...+18" resolution is not fine enough. 	- If not this, then make dt ~= 5e-2

--- src/cirs_girona_cala_viuda/scripts/camera_listener.py
+++ src/cirs_girona_cala_viuda/scripts/camera_listener.py
@@ -6,6 +6,7 @@ import sys
 class CameraListener:
     def __init__(self) -> None:
         self.file_name = f'{sys.path[0]}/camera_times.txt'
+        self.time = 0
 
     
     def callback(self, data):
@@ -15,11 +16,16 @@ class CameraListener:
         header = data.header
         time = header.stamp
 
-        file = open(self.file_name, 'a')
-        file.write(f'{time}\n')
-
-
-
+        # file = open(self.file_name, 'a')
+        # file.write(f'{time}\n')
+        print(f'Prev time: {self.time}')
+        print(f'Time raw:{time}')
+        print(f'Time sec:{time.secs}')
+        print(f'Time nsec:{time.nsecs}')
+        time_diff = (time.secs * 1e+9 + time.nsecs) - self.time
+        print(f'Time diff:{time_diff}')
+        self.time = time.secs * 1e+9 + time.nsecs
+        print()
 
 def main():
     rospy.init_node('camera_listener', anonymous=True)

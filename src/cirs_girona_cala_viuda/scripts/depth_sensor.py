Frontend fixes, need to add depth sensor and odometry, not sure how to model the Jacobian for NavState instance

src/cirs_girona_cala_viuda/scripts/depth_sensor.py
@@ -28,6 +28,8 @@ def simulate_auv() -> np.ndarray:
     dt = 0.25
     num_steps = int(10 / dt)
 
+    times = np.linspace(0, num_steps, num_steps)
+
     eta = np.zeros((6, num_steps))
     nu = np.zeros((6, num_steps))
     eta[:, 0] = eta0.reshape(6,)
@@ -44,7 +46,7 @@ def simulate_auv() -> np.ndarray:
 
     state = np.vstack((eta, nu))
 
-    return state
+    return state, times
 
 
 def depth_error(measurement: np.ndarray, this: gtsam.CustomFactor, values: gtsam.Values, jacobians: Optional[List[np.ndarray]]) -> float:
@@ -62,12 +64,13 @@ def depth_error(measurement: np.ndarray, this: gtsam.CustomFactor, values: gtsam
     return error
 
 
-def plot_states(state):
+def plot_states(state, depth_vals, traj_times):
     # Plot positions
     fig, axs = plt.subplots(2, 1, sharex=True)
     axs[0].plot(state[0, :], label='x')
     axs[0].plot(state[1, :], label='y')
     axs[0].plot(state[2, :], label='z')
+    axs[0].scatter(traj_times, depth_vals, label='depth')
     axs[0].legend()
     fig.suptitle('Position')
 
@@ -126,13 +129,13 @@ def plot_vs_gt(gt_trajectory, final_trajectory):
     plt.show()
 
 def main():
-    auv_traj = simulate_auv()
+    auv_traj, auv_times = simulate_auv()
 
     # Define noise parameters for the sensors
     odom_sigma = 0.1 * np.eye(6)
     depth_sigma = 0.1
 
-    plot_states(auv_traj)
+    # plot_states(auv_traj)
 
     # Define noise
     odom_noise = odom_sigma @ np.random.randn(6, auv_traj.shape[1])
@@ -166,13 +169,15 @@ def main():
         auv_traj[2, k] + np.random.randn() * depth_sigma for k in range(auv_traj.shape[1])
     ])
 
+    plot_states(auv_traj, depth_vals, auv_times)
+
     # When this block is commented, performs better
-    # for i in range(auv_traj.shape[1]):
-    #     depth_val = depth_vals[i]
-    #     depth = gtsam.CustomFactor(
-    #         depth_model, [unknown[i]], partial(depth_error, np.array([depth_val]))
-    #     )
-    #     graph.add(depth)
+    for i in range(auv_traj.shape[1]):
+        depth_val = depth_vals[i]
+        depth = gtsam.CustomFactor(
+            depth_model, [unknown[i]], partial(depth_error, np.array([depth_val]))
+        )
+        graph.add(depth)
 
     
     # Initialize values
@@ -201,6 +206,16 @@ def main():
         theta = np.arctan2(-rot[2,0], np.sqrt(rot[2,1]**2 + rot[2,2]**2))
         psi = np.arctan2(rot[1,0], rot[0,0])
         final_trajectory[:, i] = np.array([x, y, z, phi, theta, psi])
+    
+    res_pose = np.zeros((3, auv_traj.shape[1]))
+    for i in range(result.size()):
+        x = result.atPose3(unknown[i]).x()
+        y = result.atPose3(unknown[i]).y()
+        z = result.atPose3(unknown[i]).z()
+        res_pose[:, i] = np.array([x, y, z])
+    error_pose = np.array([(res_pose[:,k] - auv_traj[:3, k])[0] for k in range(auv_traj.shape[1])])
+
+    print('Error in position:', np.linalg.norm(error_pose))
 
     plot_vs_gt(auv_traj, final_trajectory)
 if __name__ == '__main__':

src/cirs_girona_cala_viuda/scripts/frontend/front_end.py
@@ -7,10 +7,11 @@ import numpy as np
 import pandas as pd
 from gtsam import NavState, Point3, Pose3, Rot3
 from gtsam.symbol_shorthand import B, V, X
+from typing import Optional, List
 
 from scipy.spatial.transform import Rotation as R
 
-import rospy
+from mpl_toolkits.mplot3d import Axes3D
 
 BIAS_KEY = B(0)
 DATA_DIR = f'{sys.path[0]}/../../data'
@@ -39,7 +40,7 @@ class AUVGraphSLAM:
         self.velNoise = gtsam.noiseModel.Isotropic.Sigma(3, 0.1)
         self.dt = 1e-6
 
-    def readStatesFromIEKF(self, filename):
+    def read_iekf_states(self, filename):
         '''
         Read the SE_2(3) state output by the IEKF
 
@@ -140,7 +141,26 @@ class AUVGraphSLAM:
         self.imu_times = imu_time
         self.imu = imu
 
-    def get_camera_times(self, filename):
+    def read_depth_sensor(self, filename):
+        '''
+        Read the state of the depth sensor
+
+        Inputs
+        =======
+        filename: str
+            The filename of the data
+        '''
+        file_path = os.path.join(DATA_DIR, filename)
+
+        depth_df = pd.read_csv(file_path)
+
+        depth_time = depth_df['%time'].values.astype(np.float64)
+        depth = depth_df['field.depth'].values.astype(np.float64)
+
+        self.depth_times = depth_time
+        self.depth = depth
+
+    def read_camera_times(self, filename):
         '''
         Determine the times of the camera measurements
         '''
@@ -150,6 +170,10 @@ class AUVGraphSLAM:
         times = times_df['times'].values.astype(np.float64)
 
         self.camera_times = times
+
+    #####################################################################
+    ###########################   Getters   #############################
+    #####################################################################
 
     def getNavState(self, idx):
         '''
@@ -177,15 +201,37 @@ class AUVGraphSLAM:
         state = NavState(pose, v)
         return state
 
+    #####################################################################
+    ######################## Error definitions ##########################
+    #####################################################################
+
+    def depth_error(self, measurement: np.ndarray, this: gtsam.CustomFactor, values: gtsam.Values, jacobians: Optional[List[np.ndarray]]) -> float:
+        '''
+        Calculate the error betwen the depth sensor and the depth prediction
+        '''
+        # Measurement is >0, meaning that it should be inverted
+        assert measurement <= 0, 'Measurement should be negative'
+        key = this.keys()[0]
+        estimate = values.atPose3(key)
+        depth = estimate.z()
+        error = measurement - depth
+        if jacobians is not None:
+            val = np.zeros((1,6))
+            val[2] = 1
+            jacobians[0] = val
+        return error
+            
+
     def initialize(self):
         # Extract the data
 
         print('Initializing...')
 
-        self.readStatesFromIEKF('states.csv')
+        self.read_iekf_states('states.csv')
         self.read_state_times('state_times.csv')
         self.read_imu('full_dataset/imu_adis_ros.csv')
-        self.get_camera_times('full_dataset/camera_times.csv')
+        self.read_camera_times('full_dataset/camera_times.csv')
+        self.read_depth_sensor('full_dataset/depth_sensor.csv')
 
         camera_step = self.camera_times.shape[0]
         imu_step = self.imu_times.shape[0]
@@ -198,7 +244,10 @@ class AUVGraphSLAM:
         camera_idx = 0
         imu_idx = 0
 
-        while state_idx < state_step:
+        time_elapsed = 0
+        imu_time_elapsed = 0
+
+        while state_idx < 1000:  # state_step:
             # Get the state
             # store as NavState or Pose3?
             # For now using NavState in order to use the imuPreintegrator
@@ -217,13 +266,20 @@ class AUVGraphSLAM:
                 )
                 self.initial.insert(X(state_idx), state.pose())
                 self.initial.insert(V(state_idx), state.velocity())
+
+            dt = self.state_times[state_idx] - \
+                self.state_times[state_idx - 1]
+            if dt <= 0:
+                # Skip this state, means that the udate step is made in this step
+                state_idx += 1
+                continue
             else:
-                dt = self.state_times[state_idx] - \
-                    self.state_times[state_idx - 1]
-                if dt == 0:
-                    self.dt = 5e-2
-                else:
-                    self.dt = dt
+                self.dt = dt * 1e-9
+                print('dt: ', self.dt)
+                print('Time elapsed: ', time_elapsed)
+                time_elapsed += self.dt
+                imu_time_elapsed += self.dt
+                print('Time elapsed: ', time_elapsed)
 
             if self.state_times[state_idx] > self.imu_times[imu_idx]:
                 # Get IMU measurements
@@ -239,45 +295,101 @@ class AUVGraphSLAM:
                 measuredAcc = np.array(
                     [lin_acc_x, lin_acc_y, lin_acc_z]).reshape(-1, 1)
 
+                print(f'measuredOmega {measuredOmega.T}')
+                print(f'measuredAcc {measuredAcc.T}')
+                print(f'dt {self.dt}')
+
                 self.pim.integrateMeasurement(
-                    measuredOmega, measuredAcc, self.dt)
+                    measuredOmega, measuredAcc, imu_time_elapsed)
+                imu_time_elapsed = 0
 
                 imu_idx += 1
 
-            if self.state_times[state_idx] > self.camera_times[camera_idx]:
+            # if self.state_times[state_idx] > self.camera_times[camera_idx]:
+            if time_elapsed > 1:
                 # Add factor at the time when a camera measurement is available
                 factor = gtsam.ImuFactor(X(camera_idx), V(camera_idx), X(
-                    camera_idx+1), V(camera_idx), BIAS_KEY, self.pim)
-                self.graph.push_back(factor)
+                    camera_idx+1), V(camera_idx+1), BIAS_KEY, self.pim)
+                print(f'Factor:\n{factor}')
+                self.graph.add(factor)
 
                 self.pim.resetIntegration()
 
-                rotationNoise = gtsam.Rot3.Expmap(np.random.randn(3) * 0.1)
-                translationNoise = gtsam.Point3(*np.random.randn(3) * 1)
-                poseNoise = gtsam.Pose3(rotationNoise, translationNoise)
+                self.initial.insert(X(camera_idx+1), state.pose())
+                self.initial.insert(V(camera_idx+1), state.velocity())
 
-                new_state = gtsam.NavState(
-                    state.pose().compose(poseNoise),
-                    state.velocity()
-                ) 
-
-                self.initial.insert(X(camera_idx+1), new_state.pose())
-                self.initial.insert(V(camera_idx+1), new_state.velocity())
+                time_elapsed = 0
                 camera_idx += 1
 
             state_idx += 1
 
+        self.graph.push_back(
+            gtsam.PriorFactorPose3(
+                X(camera_idx), state.pose(), self.priorNoise)
+        )
+        self.graph.push_back(
+            gtsam.PriorFactorPoint3(
+                V(camera_idx), state.velocity(), self.velNoise)
+        )
+
         self.graph.saveGraph(f'{sys.path[0]}/graph.dot', self.initial)
 
         print('Initialization complete')
-    
+
     def optimize(self):
         print('Optimizing...')
-        optimizer = gtsam.GaussNewtonOptimizer(self.graph, self.initial)
+        params = gtsam.LevenbergMarquardtParams()
+        params.setVerbosityLM("SUMMARY")
+        optimizer = gtsam.LevenbergMarquardtOptimizer(
+            self.graph, self.initial, params)
+        # optimizer = gtsam.GaussNewtonOptimizer(self.graph, self.initial)
         self.result = optimizer.optimize()
         print('Optimization complete')
+
+    def plot_trajectories(self):
+        '''
+        Compare the trajectories
+        '''
+
+        res_poses = np.zeros((self.initial.size() // 2, 3))
+        init_poses = np.zeros((self.initial.size() // 2, 3))
+        j = 0
+        for i in range(self.initial.size()):
+            if X(i) in self.initial.keys():
+                init_pose = self.initial.atPose3(X(i))
+                init_x = init_pose.x()
+                init_y = init_pose.y()
+                init_z = init_pose.z()
+
+                init_poses[j, :] = np.array([init_x, init_y, init_z])
+
+                res_pose = self.result.atPose3(X(i))
+                res_x = res_pose.x()
+                res_y = res_pose.y()
+                res_z = res_pose.z()
+
+                res_poses[j, :] = np.array([res_x, res_y, res_z])
+                j += 1
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(init_poses[:, 0], init_poses[:, 1],
+                init_poses[:, 2], label='Initial')
+        ax.scatter(init_poses[:, 0], init_poses[:, 1],
+                   init_poses[:, 2], c='r', marker='x')
+        ax.plot(res_poses[:, 0], res_poses[:, 1],
+                res_poses[:, 2], label='Result')
+        ax.scatter(res_poses[:, 0], res_poses[:, 1],
+                   res_poses[:, 2], c='b', marker='x')
+        ax.set_xlabel('X (m)')
+        ax.set_ylabel('Y (m)')
+        ax.set_zlabel('Z (m)')
+        ax.legend()
+        plt.show()
+
 
 if __name__ == '__main__':
     GraphSLAM = AUVGraphSLAM()
     GraphSLAM.initialize()
     GraphSLAM.optimize()
+    GraphSLAM.plot_trajectories()

src/cirs_girona_cala_viuda/scripts/sensors/filter_sonar.py
@@ -12,7 +12,7 @@ def wrap_to_pi(angle):
 
 class MicronVisualizer:
 
-    def __init__(self, time_interval, csv_file,time_id):
+    def __init__(self, time_interval, csv_file, time_id):
         self.time_interval = time_interval
         self.csv_file = csv_file
 
@@ -86,101 +86,84 @@ class MicronVisualizer:
         thresholded_intensities = np.copy(self.intensities)
         thresholded_intensities[thresholded_intensities < self.threshold] = 0
         #thresholded_intensities[thresholded_intensities >= self.threshold] = 1
-        thresholded_intensities[:,1:30] = 0               #elimate the intensity of the robot itself
-
+        # elimate the intensity of the robot itself
+        thresholded_intensities[:, 1:30] = 0
 
         # Set a local minimum distance for the binary mask
-        
+
        # find the max point to search other
-        m,n = thresholded_intensities.shape
+        m, n = thresholded_intensities.shape
 
         max_value = np.max(thresholded_intensities)
 
-        max_value_index = np.unravel_index(thresholded_intensities.argmax(), thresholded_intensities.shape) 
+        max_value_index = np.unravel_index(
+            thresholded_intensities.argmax(), thresholded_intensities.shape)
         thresholded_intensities[max_value_index[0]] = 0
-        thresholded_intensities[max_value_index[0], max_value_index[1]] = max_value  
-         
-           
-        #initial param that will use in search
-        search_range = 10   
-        temp_search_range = search_range    
+        thresholded_intensities[max_value_index[0],
+                                max_value_index[1]] = max_value
+
+        # initial param that will use in search
+        search_range = 10
+        temp_search_range = search_range
         temp_max = max_value
-        #initial the search point
+        # initial the search point
         line = max_value_index[0]
         temp_index = max_value_index[1]
         temp_max = max_value
         # from max value line to 0
         while line > 0:
             line -= 1
-            temp = thresholded_intensities[line, temp_index-temp_search_range:temp_index+temp_search_range]
+            temp = thresholded_intensities[line, temp_index -
+                                           temp_search_range:temp_index+temp_search_range]
             temp_max = np.max(temp)
-            # print("$$$$$$$$$$$$$$$$$$$")
-            # print(line)
-            # print(temp)
-            # print("sum")
-            # print(sum(thresholded_intensities[line]))
-            while temp_max ==0:                                             #avoid the max is zero
+            while temp_max == 0:  # avoid the max is zero
                 temp_search_range = temp_search_range + 5
-                temp = thresholded_intensities[line, temp_index-temp_search_range:temp_index+temp_search_range]
-                
+                temp = thresholded_intensities[line, temp_index -
+                                               temp_search_range:temp_index+temp_search_range]
+
                 if temp_search_range == 50:
                     break
                 temp_max = np.max(temp)
-            # print(temp_search_range)
-            # print(temp_max)
-            # print(sum(thresholded_intensities[line]))
             if temp_search_range == 50:
                 temp_search_range = search_range
                 thresholded_intensities[line] = 0
                 continue
             if temp_index < temp_search_range:
                 temp_search_range = temp_index
-                
-            temp_index = np.unravel_index(temp.argmax(), temp.shape)[0] + temp_index - temp_search_range            
+
+            temp_index = np.unravel_index(temp.argmax(), temp.shape)[
+                0] + temp_index - temp_search_range
             thresholded_intensities[line] = 0
             thresholded_intensities[line, temp_index] = temp_max
             temp_search_range = search_range
-        #reinitial the search point
+        # reinitial the search point
         line = max_value_index[0]
         temp_index = max_value_index[1]
         # from max value line to the edge
-        while line < m-1:            
+        while line < m-1:
             line += 1
-            temp = thresholded_intensities[line, temp_index-temp_search_range:temp_index+temp_search_range]
+            temp = thresholded_intensities[line, temp_index -
+                                           temp_search_range:temp_index+temp_search_range]
             temp_max = np.max(temp)
-            # print("$$$$$$$$$$$$$$$$$$$")
-            # print(line)
-            # print(temp)
-            
-            # print("sum")
-            # print(sum(thresholded_intensities[line]))
-            while temp_max ==0:                                                   #avoid the max is zero
+            while temp_max == 0:  # avoid the max is zero
                 temp_search_range = temp_search_range + 5
-                temp = thresholded_intensities[line, temp_index-temp_search_range:temp_index+temp_search_range]
-                # print("error while")
-                # print(temp_search_range)
-                # print(temp)
-                # print("range")
-                # print(temp_index-temp_search_range)
-                # print(temp_index+temp_search_range)
-                
+                temp = thresholded_intensities[line, temp_index -
+                                               temp_search_range:temp_index+temp_search_range]
+
                 if temp_search_range == 50:
                     break
                 temp_max = np.max(temp)
-            # print(temp_search_range)
-            # print(temp_max)
-            # print(sum(thresholded_intensities[line]))
             if temp_search_range == 50:
                 temp_search_range = search_range
                 thresholded_intensities[line] = 0
                 continue
             if temp_index < temp_search_range:
                 temp_search_range = temp_index
-            temp_index = np.unravel_index(temp.argmax(), temp.shape)[0] + temp_index - temp_search_range            
+            temp_index = np.unravel_index(temp.argmax(), temp.shape)[
+                0] + temp_index - temp_search_range
             thresholded_intensities[line] = 0
             thresholded_intensities[line, temp_index] = temp_max
             temp_search_range = search_range
-        # print(sum(thresholded_intensities[m-2]))
         return thresholded_intensities
 
     def plot_data(self, intensities=None, mode='polar'):
@@ -217,7 +200,7 @@ class MicronVisualizer:
                                 c=intensity[i, :], s=1, cmap='jet')
                 else:
                     plt.scatter(
-                        beam, beam_number, c=intensity[i, :], s=1, cmap='gist_yarg', vmin = 0, vmax = 1)
+                        beam, beam_number, c=intensity[i, :], s=1, cmap='gist_yarg', vmin=0, vmax=1)
                 plt.title(
                     f'Sonar data (polar coordinates)\nTime : {(self.tmp_time - self.prev_time) / 1e9:.3} seconds')
                 plt.ylabel('Beam number')
@@ -225,7 +208,7 @@ class MicronVisualizer:
         elif mode == 'cartesian':
             X = scan_range * np.cos(angles)
             Y = scan_range * np.sin(angles)
-            cmap = 'jet' if intensities is None else 'gray'
+            cmap = 'jet' if intensities is None else 'gist_yarg'
             plt.scatter(X, Y, s=1, c=intensity, cmap=cmap)
             plt.title(
                 f'Sonar data (cartesian coordinates)\nTime elapsed: {(self.tmp_time - self.prev_time) / 1e9:.3} seconds')
@@ -239,16 +222,17 @@ class MicronVisualizer:
 
 def main():
     i = 0
-    FrameNum = 50 # For debugging, keep it less else 45599
-    Threshold = 0.01
+    FrameNum = 50  # For debugging, keep it less else 45599
+    Threshold = 0.4
     while (i < FrameNum):
-        micron = MicronVisualizer(10, f'{sys.path[0]}/../../data/full_dataset/sonar_micron.csv', i)
-        i+=10 
+        micron = MicronVisualizer(
+            5, f'{sys.path[0]}/../../data/full_dataset/sonar_micron.csv', i)
+        i += 5
         micron.get_data()
         micron.set_threshold(Threshold)
-        micron.plot_data(mode='polar')
+        micron.plot_data(mode='cartesian')
         thresholded_intensities = micron.filter_sonar()
-        micron.plot_data(intensities=thresholded_intensities, mode='polar')
+        micron.plot_data(intensities=thresholded_intensities, mode='cartesian')
         # micron.plot_data(mode='cartesian')
 
 
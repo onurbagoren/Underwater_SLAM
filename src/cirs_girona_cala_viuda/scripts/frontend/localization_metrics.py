51528bc8c9063ffe8d2cd1401b8146abd3a050bb
zhongzh <zhongzh@umich.edu>
2022-04-10 15:40:33 -0400

    cleaned metrics and constants

--- src/cirs_girona_cala_viuda/scripts/frontend/localization_metrics.py
+++ src/cirs_girona_cala_viuda/scripts/frontend/localization_metrics.py
@@ -1,41 +1,7 @@
+# Modified version of https://github.com/sansaldo/IEKF_AUV_Cave_Navigation/blob/master/localization_metrics.py
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from itertools import product
-from constants import cone_times, cone_times_ordered, cone_distances, cone_path, cone_offsets_ordered, cone_offsets
+from constants import *
 
-# Calculates MSE of ground truth (gt) and predicted trajectories
-# gt and predicted should be shape (number of timesteps, number of dimensions),
-# or if different number of timesteps, pass in times objects for each of shape (number of timesteps)
-def mse(gt, predicted, gt_times=None, predicted_times=None):
-    gt = np.array(gt)
-    predicted = np.array(predicted)
-    if gt_times is not None:
-        gt_times = np.array(gt_times)
-    if predicted_times is not None:
-        predicted_times = np.array(predicted_times)
-
-    assert gt.shape[0] == predicted.shape[0] or (gt_times is not None and predicted_times is not None), "Ground truth and predicted trajectories should have same number of values. If they don't, pass their times into this function so we can match them up."
-    assert gt.shape[1] == predicted.shape[1], "Comparison results must have same number of dimensions."
-
-    if gt.shape[0] == predicted.shape[0]:
-        mse = mean_squared_error(gt.flatten(), predicted.flatten())
-    else:
-        # Only evaluate where the times overlap
-        gt_new = []
-        for t, sample in zip(gt_times, gt):
-            if t in predicted_times:
-                gt_new.append(sample)
-        gt_new = np.array(gt_new)
-
-        predicted_new = []
-        for t, sample in zip(predicted_times, predicted):
-            if t in gt_times:
-                predicted_new.append(sample)
-        predicted_new = np.array(predicted_new)
-
-        mse = mean_squared_error(gt_new.flatten(), predicted_new.flatten())
-
-    return mse
 
 # Measures 2 cone metrics from Angelos et al., 2016
 # pos: array of shape (number of pred timesteps, 3) for xyz predictions to evaluate
@@ -84,12 +50,7 @@ def cone_metrics(pos, times):
         
         # Add offset from AUV to cone
         pred1_xyz[:2] += cone_offsets_ordered[:,i]
-
-        # print('Cones',i,j)
-        # print(cone0_time, '->', cone1_time)
-        # print(cone0_idx, '->', cone1_idx)
-        # print(pred0_xyz.shape)
-
+        
         # Sanity check
         assert cone0_idx < cone1_idx
 
